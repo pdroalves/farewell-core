72d981eecd928d18e9ceb881ba79a43d8ba2fc15
Pedro Alves <pedro.alves@zama.ai>
2025-09-08 13:30:42 -0300

    Implement OpenZepellin's upgradeable pattern

---
+++ deploy/01_deploy_farewell.ts
@@ -0,0 +1,33 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { ethers, upgrades, deployments, getNamedAccounts } = hre as any;
+  const { deployer } = await getNamedAccounts();
+
+  const signer = await ethers.getSigner(deployer);
+  const Farewell = await ethers.getContractFactory("Farewell", signer);
+
+  const proxy = await upgrades.deployProxy(Farewell, [], {
+    kind: "uups",
+    initializer: "initialize",
+  });
+  await proxy.waitForDeployment();
+
+  const proxyAddr = await proxy.getAddress();
+  const implAddr = await upgrades.erc1967.getImplementationAddress(proxyAddr);
+
+  console.log("Farewell (proxy):", proxyAddr);
+  console.log("Implementation   :", implAddr);
+
+  // (Optional) Save to hardhat-deploy so deployments.get("Farewell") works
+  await deployments.save("Farewell", {
+    address: proxyAddr,
+    abi: (await hre.artifacts.readArtifact("Farewell")).abi,
+    implementation: implAddr,
+  });
+};
+
+export default func;
+func.id = "deploy_Farewell";
+func.tags = ["Farewell"];
